3af044335076ae12f487c355b9bfa674bd4a96a3
zhuliangbing <zhuliangbing@kexinjinke.com>
2019-03-26 18:13:20 +0800

    测试github配置

--- src/main/java/GenerateMessageFileTest.java
+++ src/main/java/GenerateMessageFileTest.java
@@ -1,16 +1,16 @@
 
 
-public class GenerateMessageFileTest{//extends BaseJunit4Test
+public class GenerateMessageFileTest{
+
 
     /**
-    * @Description: 测试根据 settDate 分组生成报文
-    * @author zhuliangbing
-    * @date 2018/4/12 18:49
-    */
-    public void testGroup() {
+     * @Description: 测试github配置
+     * @author zhuliangbing
+     * @date 2018/4/12 18:49
+     */
+    public static void main(String[] args) {
         System.out.println("test github");
     }
-
 
 
 
